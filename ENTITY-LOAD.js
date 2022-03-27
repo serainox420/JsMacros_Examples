b5cdacb2606e04e6978b96c5b729b4cf31cc3dca
(Silverainox) This is a stickup <95081005+serainox420@users.noreply.github.com>
2022-03-27 11:13:28 -0400

    Add files via upload
    Entity Load Event as  "Creeper Alert"

---
+++ ENTITY-LOAD.js
@@ -0,0 +1,21 @@
+
+//WORKING ONLY SOMEWHAT
+
+// Activate Creeper Alert when Creeper loads
+
+Chat.say("CREEPER ALERT") || Chat.log(event.entity.getType());
+if (event.entity.getType() == "minecraft:creeper")
+
+
+
+///Chat.log(event.entity.getName());
+///if (event.entity.getName() == "serainox")
+///Chat.say("DETECTED")
+        
+//SCANS ALL ENTITIES AND PRINTS 777 WHILE DETECTING CREEPER
+//Chat.log(event.entity.getType());
+//EntityLoad
+//if (event.entity.getType() == "minecraft:creeper") 
+//Chat.say("777")
+//World.playSoundFile("MLG.wav", 1) BEST SOUND EFFECT
+
