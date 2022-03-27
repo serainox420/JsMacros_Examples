2434603b0349a9592e310e40c0de84312189355f
(Silverainox) This is a stickup <95081005+serainox420@users.noreply.github.com>
2022-03-27 12:03:04 -0400

    Add files via upload
    Do some MLG 360's to impress noobs online

---
+++ ROTATE.js
@@ -0,0 +1,13 @@
+
+Client.waitTick(2)
+Chat.say("SMOKE WEED EVERYDAY")
+Client.waitTick(2)
+Chat.title("ROTATING", "MLG", 15, 22, 66);
+const loop_count = 500;
+for(let i = 0; i < loop_count; ++i) {
+//let blockstate = Player.rayTraceBlock(666, true).getName()//.getBlockState()
+//Chat.log(blockstate.toString())
+Chat.say(".rotation add 0 40")
+Client.waitTick(2) ;
+
+}
