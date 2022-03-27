356311d715bf295f448d6dc5642ec6bc866a9f86
(Silverainox) This is a stickup <95081005+serainox420@users.noreply.github.com>
2022-03-27 11:09:38 -0400

    Add files via upload
    Meteor's "Surround" module with JSM based "primed creeper sound" Sound Event trigger.

---
+++ ReactiveCreeperShield.js
@@ -0,0 +1,11 @@
+// Reactive Creeper Shield MKI
+// To be used with Meteor Client
+// Enable as Events -> Sound
+
+if (event.sound == 'minecraft:entity.creeper.primed')
+Chat.say(".t surround on") || Chat.log("Creeper Shield Activated")
+
+// REQUIRES TO HAVE OBSIDIAN IN HOTBAR TO WORK
+// Make sure you have "Double Height" enabled in "Surround" module settings.
+// || Chat.say(".t flamethrower on")   Optional "FireWall" 
+
