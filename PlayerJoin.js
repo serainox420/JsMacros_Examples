9be152a14488e7e9685e1e1f556c53f5646e8543
(Silverainox) This is a stickup <95081005+serainox420@users.noreply.github.com>
2022-03-27 11:11:03 -0400

    Add files via upload
    Player Join Event looking for UUID

---
+++ PlayerJoin.js
@@ -0,0 +1,4 @@
+//new EventPlayerJoin (59dc4f0b-96c8-48a1-961a-ee41fc183490, ViscousLiveResin)
+// Run Name.js if player UUID joined.
+if (event.player.getString() == UUID)
+JsMacros.runScript("Name.js")
